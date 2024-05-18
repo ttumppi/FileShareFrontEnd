3b3f11af86624c9948da28cf77ff13de8d40639a
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-05-18 18:29:23 +0300

    Added navigation to home screen.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 
 const GetRequested = () =>{
   console.log("Get Request Has been made");
+  useNavigate("/");
   return (<h1>Message Received</h1>);
 }
 
 
-const TestElement = () => (
+const PageNameHeader = () => (
   <div className="header-grid">
     <div className="header-item">FileShare</div>
   </div>
@@ -22,7 +23,7 @@ const TestElement = () => (
 const MainPage = () =>(
   <Router>
     <div>
-      <TestElement />
+      <PageNameHeader />
       <Routes>
         <Route exact path="/test" element={<GetRequested />} />
       </Routes>
