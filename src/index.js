89fd418d49a21cc9d17d02be90184a78f15c2acc
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-05-18 17:07:06 +0300

    Added Basic Routing to /test not tested.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
+
+const testElement = 
+<div className="header-grid">
+  <div className="header-item">FileShare</div>
+</div>
+
+const route =
+<div>
+  <Route exact path="/test" component={<h1>Message Received</h1>} />
+</div>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  testElement,
+  <Router>
+    route
+    </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
