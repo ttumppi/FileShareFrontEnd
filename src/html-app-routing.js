eb1a1639b282d395c866a3ec7a05b6edba3a4752
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-05-19 02:01:20 +0300

    Added logs to requests.

--- src/html-app-routing.js
+++ src/html-app-routing.js
@@ -1,11 +1,13 @@
 import {app} from "./html-app";
 
 app.get('/', (req, res) => {
+    console.log("homepage get requested");
     const html = res.render("homepage");
     return html;
   });
   
   app.get('/about', (req, res) => {
+    console.log("About get request sent");
     const html = res.render("errorpage");
     return html;
   });
