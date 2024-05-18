70505ce5208d8ab4b044c4902eba2a2e35f89f75
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-05-18 17:30:48 +0300

    Fixed Routing

--- src/index.js
+++ src/index.js
@@ -3,22 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 
-const testElement = 
-<div className="header-grid">
-  <div className="header-item">FileShare</div>
-</div>
 
-const route =
-<div>
-  <Route exact path="/test" component={<h1>Message Received</h1>} />
-</div>
+const GetRequested = () =>{
+  <h1>Message Received</h1>;
+}
+
+
+const TestElement = () => {
+  <div className="header-grid">
+    <div className="header-item">FileShare</div>
+  </div>
+}
+
+
+
+
+const MainPage = () =>{
+  <Router>
+  <div>
+  <TestElement />
+  <Switch>
+    <Route exact path="/test" component={GetRequested} />
+  </Switch>
+  </div>
+</Router>
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  testElement,
-  <Router>
-    route
-    </Router>
+  < MainPage />
 );
 
 
