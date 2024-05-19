b39c1cf0f94b6502f3918bfd29b2e7025f135a99
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-05-19 03:22:24 +0300

    ss

--- src/html-app.js
+++ src/html-app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import path from "path";
 import { fileURLToPath } from 'url';
-import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
