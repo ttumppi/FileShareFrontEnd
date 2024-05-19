dc388e4d08a43d6830d6e7716da882a153f070ed
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-05-19 03:21:34 +0300

    sssdaaa

--- src/html-app.js
+++ src/html-app.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import path from "path";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.set("view engine", "ejs");
