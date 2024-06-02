2ca9e09cdcaede4888746e319639be922c7212ea
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-06-02 20:50:02 +0300

    Added form to main page.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,63 @@
+import { createRoutesFromElements } from "react-router-dom";
+import * as AddressConstants from "./constants.js" ;
+
+const inputFieldNames = ["username", "password", "loginButton"]; 
+
+
 const container = document.getElementById("input-container");
 
-const inputField = document.createElement("input");
+const form = document.createElement("form");
+form.action = AddressConstants.POST_LOGIN_CREDENTIALS;
+form.method = "POST";
 
-inputField.type = "text";
+for (i = 0; i < inputFieldNames.length; i++){
+    if (inputFieldNames[i] == "username"){
+        form.appendChild(createUsernameInputField());
+        continue;
+    }
+    if (inputFieldNames[i] == "password"){
+        form.appendChild(createPassswordInputField());
+        continue;
+    }
+    if (inputFieldNames[i] == "loginButton"){
+        form.appendChild(createSubmitButton());
+        continue;
+    }
 
-inputField.id = "input-field";
+}
 
-inputField.placeholder = "Username...";
+container.appendChild(form);
 
-container.appendChild(inputField);
 
+
+
+
+const createUsernameInputField = () => {
+    const inputField = document.createElement("input");
+
+    inputField.type = "text";
+    inputField.id = "usernameInput";
+    inputField.placeholder = "Username...";
+
+    return inputField;
+}
+
+const createPasswordInputField = () => {
+    const inputField = document.createElement("input");
+
+    inputField.type = "password";
+    inputField.id = "passwordInput";
+    inputField.placeholder = "";
+
+    return inputField;
+}
+
+const createSubmitButton = () => {
+    const button = document.createElement("input");
+
+    button.type = "submit";
+    button.id = "submitButton";
+    button.value = "Login";
+
+    return button;
+}
