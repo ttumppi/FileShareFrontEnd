120dcf0eb7dcaadb6615765c099f762a3db83abb
jeussi <142884208+jeussi@users.noreply.github.com>
2024-06-02 16:43:31 +0300

    fixed script code

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById("input-container");
 
-const inputField = document.createElement();
+const inputField = document.createElement("input");
 
 inputField.type = "text";
 
