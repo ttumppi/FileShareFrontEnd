0fe951d226a31cec71a22159e76ca4d191fbe7e6
jeussi <142884208+jeussi@users.noreply.github.com>
2024-06-02 16:03:41 +0300

    username input

---
+++ src/script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("input-container");
+
+const inputField = document.createElement
+
+inputField.type = "text";
+
+inputfield.id = "input-field";
+
+inputField.placeholder = "Username...";
+
