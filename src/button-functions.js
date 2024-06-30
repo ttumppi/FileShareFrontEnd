369c7c84816d2eadc6fe2c112acac96cfb8a0f31
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-06-30 04:41:47 +0300

    Added redirect to button click.

--- src/button-functions.js
+++ src/button-functions.js
@@ -1,5 +1,6 @@
 const OnDownloadClick = (id) => {
-    fetch("http://dabblestudios.tplinkdns.com:16000/download/" + id);
+    //fetch("http://dabblestudios.tplinkdns.com:16000/download/" + id);
+    window.location.href = "http://dabblestudios.tplinkdns.com:16000/download/" + id
 }
 
 
