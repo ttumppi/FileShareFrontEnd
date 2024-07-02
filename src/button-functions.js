0a467690ed019ff282e62f43691fb127b2c4d311
jeussi <142884208+jeussi@users.noreply.github.com>
2024-07-03 02:19:17 +0300

    https

--- src/button-functions.js
+++ src/button-functions.js
@@ -1,6 +1,5 @@
 const OnDownloadClick = (id) => {
-    //fetch("http://dabblestudios.tplinkdns.com:16000/download/" + id);
-    window.location.href = "http://dabblestudios.tplinkdns.com:16000/download/" + id
+    window.location.href = "https://dabblestudios.tplinkdns.com:16000/download/" + id
 }
 
 const  OnUploadClick = async () => {
